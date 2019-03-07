732a85f2b31a7738356b19d2551b52b2bb2d6e06
istar <riyamitra@istarindia.com>
2019-03-07 21:31:50 +0530

    Notification

--- app/src/main/java/com/example/myapplication/notif/NotificationActivity.java
+++ app/src/main/java/com/example/myapplication/notif/NotificationActivity.java
@@ -17,6 +17,7 @@ import android.support.v4.app.RemoteInput;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
@@ -26,6 +27,8 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class NotificationActivity extends AppCompatActivity {
+    @BindView(R.id.comment)
+    public TextView notifComment;
     public static final String PRIMARY_CHANNEL = "default";
     public static final String SECONDARY_CHANNEL = "secondary channel";
 
@@ -34,6 +37,9 @@ public class NotificationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
         ButterKnife.bind(this);
+        if (getIntent() != null) {
+            notifComment.setText(getIntent().getStringExtra("reply"));
+        }
     }
 
     @OnClick(R.id.notifButton)
@@ -77,7 +83,6 @@ public class NotificationActivity extends AppCompatActivity {
             NotificationManager notificationManager1 = getSystemService(NotificationManager.class);
             notificationManager1.createNotificationChannel(channel);
 
-
         }
 // notificationId is a unique int for each notification that you must define
         notificationManager.notify(123, mBuilder.build());
@@ -119,6 +124,50 @@ public class NotificationActivity extends AppCompatActivity {
                 PendingIntent.FLAG_UPDATE_CURRENT
         );
 
+        //Creating the notification builder object
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, NotificationKeyStore.CHANNNEL_ID)
+                .setSmallIcon(R.mipmap.app_icon)
+                .setContentTitle("New Lead Added")
+                .setContentText("Associate added a lead.")
+                .setAutoCancel(true)
+                .setColor(getResources().getColor(R.color.theme_color))
+                .setContentIntent(dissaprovePendingIntent)
+                .addAction(R.drawable.ic_done_white_24dp, "Approve", approvePendingIntent)
+                .addAction(R.drawable.ic_close_white_24dp, "Disapprove", dissaprovePendingIntent);
+                //.addAction(action);
+
+
+        //finally displaying the notification
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NotificationKeyStore.NOTIFICATION_ID, mBuilder.build());
+
+    }
+
+    @OnClick(R.id.twoButton)
+    public void twoButtonNotification(){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationManager mNotificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(NotificationKeyStore.CHANNNEL_ID, NotificationKeyStore.CHANNEL_NAME, importance);
+            channel.setDescription(NotificationKeyStore.CHANNEL_DESC);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mNotificationManager.createNotificationChannel(channel);
+        }
+
+        Intent intent = new Intent(this, NotificationActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                NotificationActivity.this,
+                NotificationKeyStore.REQUEST_CODE_APPROVE,
+                new Intent(NotificationActivity.this, NotificationBroadcastReceiver.class)
+                        .putExtra(NotificationKeyStore.KEY_INTENT_APPROVE, NotificationKeyStore.REQUEST_CODE_APPROVE),
+                PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
         //We need this object for getting direct input from notification
         RemoteInput remoteInput = new RemoteInput.Builder(NotificationKeyStore.NOTIFICATION_REPLY)
                 .setLabel("Comment...")
@@ -127,8 +176,8 @@ public class NotificationActivity extends AppCompatActivity {
 
         //For the remote input we need this action object
         NotificationCompat.Action action =
-                new NotificationCompat.Action.Builder(android.R.drawable.ic_delete,
-                        "Add A Comment", dissaprovePendingIntent)
+                new NotificationCompat.Action.Builder(R.drawable.ic_done_white_24dp,
+                        "Add A Comment", pendingIntent)
                         .addRemoteInput(remoteInput)
                         .build();
 
@@ -138,20 +187,16 @@ public class NotificationActivity extends AppCompatActivity {
                 .setContentTitle("New Lead Added")
                 .setContentText("Associate added a lead.")
                 .setAutoCancel(true)
-                .setContentIntent(dissaprovePendingIntent)
-                .addAction(R.drawable.ic_done_white_24dp, "Approve", approvePendingIntent)
-                .addAction(R.drawable.ic_close_white_24dp, "Disapprove", dissaprovePendingIntent)
-                .addAction(action);
+                .setColor(getResources().getColor(R.color.theme_color))
+                //.setContentIntent(pendingIntent);
+               .addAction(action);
 
 
         //finally displaying the notification
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.notify(NotificationKeyStore.NOTIFICATION_ID, mBuilder.build());
 
+
     }
 
 }
-
-
-
-
--- app/src/main/java/com/example/myapplication/notif/NotificationBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplication/notif/NotificationBroadcastReceiver.java
@@ -1,16 +1,15 @@
 package com.example.myapplication.notif;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.app.RemoteInput;
 import android.widget.Toast;
 
-import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
 
 
@@ -19,31 +18,98 @@ public class NotificationBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         //getting the remote input bundle from intent
-        Bundle remoteInput = RemoteInput.getResultsFromIntent(intent);
+        Bundle bundleremoteInput = RemoteInput.getResultsFromIntent(intent);
 
         //if there is some input
-        if (remoteInput != null) {
+        if (bundleremoteInput != null) {
 
             //getting the input value
-            CharSequence reply = remoteInput.getCharSequence(NotificationKeyStore.NOTIFICATION_REPLY);
+            String reply = (bundleremoteInput.getCharSequence(NotificationKeyStore.NOTIFICATION_REPLY)).toString();
+            intent = new Intent(context, NotificationActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra("reply", reply);
 
-            //updating the notification with the input value
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, NotificationKeyStore.CHANNNEL_ID)
-                    .setSmallIcon(android.R.drawable.ic_menu_info_details)
+                    .setSmallIcon(R.drawable.app_icon_white)
+                    .setColor(context.getResources().getColor(R.color.theme_color))
                     .setContentTitle("Comment: " + reply);
             NotificationManager notificationManager = (NotificationManager) context.
                     getSystemService(Context.NOTIFICATION_SERVICE);
             notificationManager.notify(NotificationKeyStore.NOTIFICATION_ID, mBuilder.build());
+
+            context.startActivity(intent);
+
         }
+
 
         //if disaprove button is clicked
         if (intent.getIntExtra(NotificationKeyStore.KEY_INTENT_DISAPPROVE, -1) == NotificationKeyStore.REQUEST_CODE_DISAPPROVE) {
-            Toast.makeText(context, "Lead Dissaproved", Toast.LENGTH_LONG).show();
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                    context,
+                    NotificationKeyStore.REQUEST_CODE_DISAPPROVE,
+                    new Intent(context, NotificationBroadcastReceiver.class)
+                            .putExtra(NotificationKeyStore.KEY_INTENT_DISAPPROVE, NotificationKeyStore.REQUEST_CODE_DISAPPROVE),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
+
+            NotificationCompat.Action action = setAction(pendingIntent);
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, NotificationKeyStore.CHANNNEL_ID)
+                    .setSmallIcon(R.drawable.app_icon_white)
+                    .setContentTitle("Lead Disapproved")
+                    .setContentText("Would you like to add a comment?")
+                    .setColor(context.getResources().getColor(R.color.theme_color))
+                    .addAction(action);
+
+            NotificationManager notificationManager = (NotificationManager) context.
+                    getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(NotificationKeyStore.NOTIFICATION_ID, mBuilder.build());
+
+
+            Toast.makeText(context, "Lead has been Dissaproved", Toast.LENGTH_LONG).show();
         }
 
         //if approve button is clicked
         if (intent.getIntExtra(NotificationKeyStore.KEY_INTENT_APPROVE, -1) == NotificationKeyStore.REQUEST_CODE_APPROVE) {
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                    context,
+                    NotificationKeyStore.REQUEST_CODE_APPROVE,
+                    new Intent(context, NotificationBroadcastReceiver.class)
+                            .putExtra(NotificationKeyStore.KEY_INTENT_APPROVE, NotificationKeyStore.REQUEST_CODE_APPROVE),
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
+
+            NotificationCompat.Action action = setAction(pendingIntent);
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context, NotificationKeyStore.CHANNNEL_ID)
+                    .setSmallIcon(R.drawable.app_icon_white)
+                    .setContentTitle("Lead has been Approved")
+                    .setContentText("Would you like to add a comment?")
+                    .setColor(context.getResources().getColor(R.color.theme_color))
+                    .addAction(action);
+
+            NotificationManager notificationManager = (NotificationManager) context.
+                    getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.notify(NotificationKeyStore.NOTIFICATION_ID, mBuilder.build());
+
+
             Toast.makeText(context, "Lead Approved", Toast.LENGTH_LONG).show();
         }
     }
+
+    public NotificationCompat.Action setAction(PendingIntent pendingIntent) {
+        RemoteInput remoteInput = new RemoteInput.Builder(NotificationKeyStore.NOTIFICATION_REPLY)
+                .setLabel("Comment...")
+                .build();
+
+        //For the remote input we need this action object
+        NotificationCompat.Action action =
+                new NotificationCompat.Action.Builder(R.drawable.ic_done_white_24dp,
+                        "Add A Comment", pendingIntent)
+                        .addRemoteInput(remoteInput)
+                        .build();
+        return action;
+    }
+
 }
